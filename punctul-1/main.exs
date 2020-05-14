3a85a25233946b0c639d40e101ea537ab2ee48dc
EINazare <emanuelioannazare@gmail.com>
2020-05-14 17:38:04 +0300

    refactor: change code to something much simpler
    I am trying to only have 3 processes, one for the Dragon, one for the 
    Necromancer. At the moment, I am yet unable to make them communicate 
    with one and other.

--- punctul-1/main.exs
+++ punctul-1/main.exs
@@ -1,25 +1,68 @@
-processReciever = self()
-
-defmodule NecromancerStrategy do
-  def run(processReciever) do
+defmodule Necromancer do
+  def start_link(pid) do
+    Task.start_link(fn -> recieve_attack(10000) end)
+    Task.start_link(fn -> send_attack(pid) end)
+  end
+  defp recieve_attack(life) do
+    cond do
+      life > 0 ->
+        receive do
+          {:Whiptail, quantity} -> recieve_attack(life - quantity)
+        end
+      life < 0 ->
+        IO.puts "Necromancer has died"
+      life == 0 ->
+        IO.puts "Necromancer has died"
+    end
+  end
+  defp send_attack(pid) do
     random = Enum.random(0..1000)
-    send processReciever, {:Anti_zombie_bolt,random}
+    send pid, {:Anti_zombie_bolt, random}
+    send_attack(pid)
   end
 end
 
-defmodule DragonStrategy do
-  def run(processReciever) do
-    random = Enum.random(50..100)
-    send processReciever, {:Whiptail,random}
+defmodule Dragon do
+  def start_link(pid) do
+    Task.start_link(fn -> recieve_attack(1000000) end)
+    Task.start_link(fn -> send_attack(pid) end)
+  end
+  defp recieve_attack(life) do
+    cond do
+      life > 0 ->
+        receive do
+          {:Anti_zombie_bolt, quantity} -> recieve_attack(life - quantity)
+        end
+      life < 0 ->
+        IO.puts "Dragon has died"
+      life == 0 ->
+        IO.puts "Dragon has died"
+    end
+  end
+  defp send_attack(pid) do
+    random = Enum.random(0..1000)
+    send pid, {:Whiptail, random}
+    send_attack(pid)
   end
 end
 
-spawn(NecromancerStrategy,:run,[processReciever])
-spawn(DragonStrategy,:run,[processReciever])
+defmodule Server do
+  def start_link do
+    {:ok, dragonPid} = Dragon.start_link(self())
+    {:ok, necromancerPid} = Necromancer.start_link(self())
+    Task.start_link(fn -> loop(dragonPid,necromancerPid) end)
+  end
+  defp loop(dragonPid,necromancerPid) do
+    receive do
+      {:Whiptail, quantity} ->
+        send dragonPid, {:Whiptail, quantity}
+        loop(dragonPid,necromancerPid)
+      {:Anti_zombie_bolt, quantity} ->
+        send necromancerPid, {:Anti_zombie_bolt, quantity}
+        loop(dragonPid,necromancerPid)
+    end
+  end
+end
 
-receive do
-  {:Anti_zombie_bolt, msg} -> IO.puts "Necromancer hits with " <> to_string(msg)
-end
-receive do
-  {:Whiptail, msg} -> IO.puts "Dragon hits with " <> to_string(msg)
-end
+
+Server.start_link
