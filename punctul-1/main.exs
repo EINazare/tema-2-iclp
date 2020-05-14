0c327f5ebe1085cae1e8cae3a23c7f66120056f5
EINazare <emanuelioannazare@gmail.com>
2020-05-14 16:33:52 +0300

    feature: add DragonStrategy

--- punctul-1/main.exs
+++ punctul-1/main.exs
@@ -3,12 +3,23 @@ processReciever = self()
 defmodule NecromancerStrategy do
   def run(processReciever) do
     random = Enum.random(0..1000)
-    send processReciever, {random}
+    send processReciever, {:Anti_zombie_bolt,random}
+  end
+end
+
+defmodule DragonStrategy do
+  def run(processReciever) do
+    random = Enum.random(50..100)
+    send processReciever, {:Whiptail,random}
   end
 end
 
 spawn(NecromancerStrategy,:run,[processReciever])
+spawn(DragonStrategy,:run,[processReciever])
 
 receive do
-  {msg} -> IO.puts msg
+  {:Anti_zombie_bolt, msg} -> IO.puts "Necromancer hits with " <> to_string(msg)
+end
+receive do
+  {:Whiptail, msg} -> IO.puts "Dragon hits with " <> to_string(msg)
 end
