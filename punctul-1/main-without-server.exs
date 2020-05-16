0bc126a459524ab89a8ba783d55d5c9ea79f1941
EINazare <emanuelioannazare@gmail.com>
2020-05-16 19:54:50 +0300

    refactor: rename pids

--- punctul-1/main-without-server.exs
+++ punctul-1/main-without-server.exs
@@ -2,30 +2,30 @@ defmodule Necromancer do
   def start_link do
     Task.start_link(fn -> recieve_attack(10000,nil,nil) end)
   end
-  defp recieve_attack(life,pid,send_attack_pid) do
+  defp recieve_attack(life,dragon_pid,send_attack_pid) do
     cond do
       life > 0 ->
         receive do
-          {:Dragon_pid, pid} ->
-            {:ok, send_attack_pid} = Task.start_link(fn -> send_attack(pid) end)
-            recieve_attack(life,pid,send_attack_pid)
+          {:Dragon_pid, dragon_pid} ->
+            {:ok, send_attack_pid} = Task.start_link(fn -> send_attack(dragon_pid) end)
+            recieve_attack(life,dragon_pid,send_attack_pid)
           {:Whiptail, quantity} ->
-            recieve_attack(life - quantity,pid,send_attack_pid)
+            recieve_attack(life - quantity,dragon_pid,send_attack_pid)
           {:Dragon_has_died} ->
             IO.puts "A castigat necromancer-ul si a ramas cu " <> to_string(life) <> " cantitate de viata."
             Process.exit(send_attack_pid, :kill)
             Process.exit(self(), :normal)
         end
       life < 0 || life == 0 ->
-        send pid, {:Necromancer_has_died}
+        send dragon_pid, {:Necromancer_has_died}
         Process.exit(send_attack_pid, :kill)
         Process.exit(self(), :normal)
     end
   end
-  defp send_attack(pid) do
+  defp send_attack(dragon_pid) do
     random = Enum.random(0..1000)
-    send pid, {:Anti_zombie_bolt, random}
-    send_attack(pid)
+    send dragon_pid, {:Anti_zombie_bolt, random}
+    send_attack(dragon_pid)
   end
 end
 
@@ -33,30 +33,30 @@ defmodule Dragon do
   def start_link do
     Task.start_link(fn -> recieve_attack(1000000,nil,nil) end)
   end
-  defp recieve_attack(life,pid,send_attack_pid) do
+  defp recieve_attack(life,necromancer_pid,send_attack_pid) do
     cond do
       life > 0 ->
         receive do
-          {:Necromancer_pid, pid} ->
-            {:ok, send_attack_pid} = Task.start_link(fn -> send_attack(pid) end)
-            recieve_attack(life,pid,send_attack_pid)
+          {:Necromancer_pid, necromancer_pid} ->
+            {:ok, send_attack_pid} = Task.start_link(fn -> send_attack(necromancer_pid) end)
+            recieve_attack(life,necromancer_pid,send_attack_pid)
           {:Anti_zombie_bolt, quantity} ->
-            recieve_attack(life - quantity,pid,send_attack_pid)
+            recieve_attack(life - quantity,necromancer_pid,send_attack_pid)
           {:Necromancer_has_died} ->
             IO.puts "A castigat dragonul si a ramas cu " <> to_string(life) <> " cantitate de viata."
             Process.exit(send_attack_pid, :kill)
             Process.exit(self(), :normal)
           end
       life < 0 || life == 0 ->
-        send pid, {:Dragon_has_died}
+        send necromancer_pid, {:Dragon_has_died}
         Process.exit(send_attack_pid, :kill)
         Process.exit(self(), :normal)
     end
   end
-  defp send_attack(pid) do
+  defp send_attack(necromancer_pid) do
     random = Enum.random(50..100)
-    send pid, {:Whiptail, random}
-    send_attack(pid)
+    send necromancer_pid, {:Whiptail, random}
+    send_attack(necromancer_pid)
   end
 end
 
